c0b2b1e90a1f987b860c803a030fca6a22b8828b
cdv123 <charles.duboisveltman@gmail.com>
2024-09-29 20:29:58 +0200

    Add efficiency results

--- one_node_results/plot_results.py
+++ one_node_results/plot_results.py
@@ -13,17 +13,33 @@ for processor_num in processor_nums:
     results.append(result.stdout)
     results[-1] = float(results[-1][:-2])
 
-print(results)
-print(len(results))
-
 plt.scatter(processor_nums, results)
 m, b = np.polyfit(processor_nums, results, 1)
 plt.plot(processor_nums, [m * xi + b for xi in processor_nums], color='red', label='Best Fit Line')
 
 plt.xlabel('Number of processors')
-plt.ylabel('Time')
-plt.title('NAMD run on varying number of processors')
+plt.ylabel('ns per day')
+plt.title('Efficiency for NAMD run on varying number of processors')
 
-plt.savefig("one_node_graph.png")
+plt.savefig("one_node_graph_time.png")
 
-plt.show()
+plt.clf()
+
+time_to_core_ratio = [time/num_processor for time, num_processor
+                      in zip(results, processor_nums)]
+
+most_efficient = max(time_to_core_ratio)
+
+efficieny = [ratio / most_efficient for ratio in time_to_core_ratio]
+print(efficieny)
+
+plt.scatter(processor_nums, efficieny)
+
+m, b = np.polyfit(processor_nums, efficieny, 1)
+plt.plot(processor_nums, [m * xi + b for xi in processor_nums], color='red', label='Best Fit Line')
+
+plt.xlabel('Number of processors')
+plt.ylabel('Efficiency')
+
+plt.title('Efficency for NAMD run on varying number of cores')
+plt.savefig("one_node_graph_efficiency.png")
